63f6076b0d611fd222b061ff23d328101e98a322
Rohalio Newman <jabwoy16@yahoo.com>
2026-04-16 10:00:15 -0700

    prompt for number

--- script.js
+++ script.js
@@ -1,21 +1,56 @@
+function loadGuessNumber() {
+   //Generate a number between 1 and 10
+   let randomNumber = Math.floor(Math.random() * 10) + 1;
+   console.log("Random Number:", randomNumber);
+
+   let attempts = 0;
+   // initializing the undefined guess variable
+   let guess;
+   
+   while (guess !== randomNumber) {
+
+      //Prompting a user for a number
+      guess = Number(prompt("Guess a number between 1 and 10:"))
+
+      //incriment the attemps counter
+      attempts ++
+
+      //check if the guess is correct
+      if (guess > randomNumber) {
+         console.log("Too high! try again.");
+      }
+      else if (guess < randomNumber) {
+         console.log("Too low! Try again.");
+      }
+      else {
+         console.log(`Congratulations! You guessed the number in ${attempts} attempts. The number was ${randomNumber}.`)
+      }
+   }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
    console.log("Hello from my About Me page!");
-   
-   
-document.getElementById('calculate').addEventListener('click', function() {
-   
-   //get values from the input fields
-   let inputNum1 = document.getElementById('num1').value;
-   console.log("inputNum1", inputNum1);
-   let num1 = Number(inputNum1.value);
 
-   let num2 = Number(document.getElementById('num2').value);
-   let sum = num1 + num2;
+   //LOADS GUESS NUMBER GAME.
+   loadGuessNumber();
 
-   let results = document.getElementById('result')
-   console.log("results", results);
-   results.innerHTML = "sum: " + sum;
+   let calButton = document.getElementById('calculate');
 
-   console.log("sum: ", sum);
-});
+   if (calButton) {
+      calButton.addEventListener('click', function(){
+         //get values from the input fields
+         let inputNum1 = document.getElementById('num1');
+         console.log("inputNum1", inputNum1);
+         let num1 = Number(inputNum1.value);
+
+         let num2 = Number(document.getElementById('num2').value);
+         let sum = num1 + num2;
+
+         let results = document.getElementById('result')
+         console.log("results", results);
+         results.innerHTML = "sum: " + sum;
+
+         console.log("sum: ", sum);
+      });
+   }   
 });
