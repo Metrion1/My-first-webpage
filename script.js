0d33b3990fb4227467c364d0601c16b0f10343b2
Rohalio Newman <jabwoy16@yahoo.com>
2026-04-23 10:39:25 -0700

    Added tip calculator form and functions

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function addNumbers(num1, num2) {
+   console.log(num1, num2);
    return num1 + num2;
 }
 
@@ -10,7 +11,7 @@ function loadGuessNumber() {
    let attempts = 0;
    // initializing the undefined guess variable
    let guess;
-   
+
    let sum = addNumbers(3, 2);
    console.log('Sum at loadGuessNumner', sum);
    while (guess !== randomNumber) {
@@ -19,7 +20,7 @@ function loadGuessNumber() {
       guess = Number(prompt("Guess a number between 1 and 10:"))
 
       //incriment the attemps counter
-      attempts ++
+      attempts++
 
       //check if the guess is correct
       if (guess > randomNumber) {
@@ -32,31 +33,55 @@ function loadGuessNumber() {
          console.log(`Congratulations! You guessed the number in ${attempts} attempts. The number was ${randomNumber}.`)
       }
    }
-}
+};
 
-document.addEventListener('DOMContentLoaded', function() {
+const calculateTip = (billAmount, tipPercentage) => {
+   if (typeof billAmount !== "number" ||
+      typeof tipPercentage !== "number" ||
+      billAmount < 0 ||
+      tipPercentage < 0) {
+      return "invalid input. Please enter numbers for bill amount and tip percentage.";
+   }
+
+   return (billAmount + (billAmount * (tipPercentage / 100))).toFixed(2);
+};
+
+document.addEventListener('DOMContentLoaded', function () {
    console.log("Hello from my About Me page!");
 
    //LOADS GUESS NUMBER GAME.
    //loadGuessNumber();
-
+   let results = document.getElementById('results')
    let calButton = document.getElementById('calculate');
+   let tipForm = document.getElementById("tip-form");
+
+   if (tipForm) {
+      tipForm.addEventListener("submit", (event) => {
+         // to prevent the page from reloading.
+         event.preventDefault();
+
+         let billAmount = Number(document.getElementById("billAmount").value);
+         let TipPercentage = Number(document.getElementById("TipPercentage").value);
+         results.innerHTML = calculateTip(billAmount, TipPercentage);
+      });
+   }
 
    if (calButton) {
-      calButton.addEventListener('click', function(){
+      calButton.addEventListener('click', function (event) {
+         console.log(event);
          //get values from the input fields
          let inputNum1 = document.getElementById('num1');
-         console.log("inputNum1", inputNum1);
+         console.log("inputNum1", inputNum1.value);
          let num1 = Number(inputNum1.value);
 
          let num2 = Number(document.getElementById('num2').value);
          let sum = addNumbers(num1, num2);
 
-         let results = document.getElementById('result')
+
          console.log("results", results);
          results.innerHTML = "sum: " + sum;
 
          console.log("sum: ", sum);
       });
-   }   
+   }
 });
